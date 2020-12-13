a2326574f9bc3143a3cab8caed0462aa6110b1ad
Moncef <moncef.koumila@gmail.com>
2020-12-13 17:05:03 +0000

    Add styling to the lists

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,11 +2,12 @@ import React from "react";
 import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos }) => {
+  // Map over the list of videos from YouTube API and pass each video's data to VideoItem component as a prop
   const renderedList = videos.map((video) => {
     return <VideoItem video={video} />;
   });
 
-  return <div>{renderedList}</div>;
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
