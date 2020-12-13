0c325ed7d8b1b902cdc2b85dc03f45dc7b5418aa
Moncef <moncef.koumila@gmail.com>
2020-12-13 17:32:28 +0000

    Dsiplaying a video player

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,8 +4,15 @@ const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>Search for something</div>;
   }
+
+  // Selected video URL
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
     <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} />
+      </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
         <p>{video.snippet.description}</p>
