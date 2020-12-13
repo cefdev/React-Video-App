90404cbc21fd057d4317f00450c4af57ac8bb033
Moncef <moncef.koumila@gmail.com>
2020-12-13 16:01:56 +0000

    Build SearchBar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  state = { term: "" };
+
+  onIputChange = (event) => {
+    this.setState({ term: event.target.value });
+    console.log(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onIputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
