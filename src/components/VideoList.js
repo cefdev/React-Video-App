e5cdb14691f2833f75b307bbd485ec9487850954
Moncef <moncef.koumila@gmail.com>
2020-12-15 20:26:08 +0000

    Fixing bug: Channels removed from displayed list

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,13 +4,15 @@ import VideoItem from "./VideoItem";
 const VideoList = ({ videos, onVideoSelect }) => {
   // Map over the list of videos from YouTube API and pass each video's data to VideoItem component as a prop
   const renderedList = videos.map((video) => {
-    return (
-      <VideoItem
-        video={video}
-        onVideoSelect={onVideoSelect}
-        key={video.id.videoId}
-      />
-    );
+    if (!video.id.channelId) {
+      return (
+        <VideoItem
+          video={video}
+          onVideoSelect={onVideoSelect}
+          key={video.id.videoId}
+        />
+      );
+    }
   });
 
   return <div className="ui relaxed divided list">{renderedList}</div>;
